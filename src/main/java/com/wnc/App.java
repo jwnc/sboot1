f537f519135acda0ec2af036cf8c44657e7fb9f3
jwnc <983855810@qq.com>
2018-10-09 00:26:41 +0800

    直播吧评论支持分类查询

--- src/main/java/com/wnc/App.java
+++ src/main/java/com/wnc/App.java
@@ -7,23 +7,26 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-@EnableScheduling
-@EnableJpaRepositories(basePackages={"com.wnc"})
+// @EnableScheduling
+@EnableJpaRepositories( basePackages = {"com.wnc"} )
 @EnableJpaAuditing
 // @EnableTransactionManagement
-public class App extends SpringBootServletInitializer {
+public class App extends SpringBootServletInitializer
+{
     @Override
     protected SpringApplicationBuilder configure(
-            SpringApplicationBuilder application) {
-        return application.sources(App.class);
+            SpringApplicationBuilder application )
+    {
+        return application.sources( App.class );
     }
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run( App.class, args );
     }
 }
